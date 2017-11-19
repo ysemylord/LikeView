fad4d22d40adafbc486b1e97b6dcb060ea4d5b83
xuyabo <1585854768@qq.com>
2017-11-19 12:16:51 +0800

    光圈透明度变化

--- app/src/main/java/com/xyb/myapplication/ui/ThumbView.java
+++ app/src/main/java/com/xyb/myapplication/ui/ThumbView.java
@@ -9,16 +9,21 @@ import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
 import com.xyb.myapplication.R;
+
+import static android.R.attr.max;
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by xuyabo on 2017/10/28.
  */
 
 public class ThumbView extends View{
+    String TAG="ThumbView";
     private Bitmap mThumbBitmap;
     private Bitmap mNotThumbBitmap;
     private Bitmap mShinerBitmap;
@@ -32,7 +37,10 @@ public class ThumbView extends View{
     public final static  int THUMB=1;
     private int status=NOT_THUMB;
 
-    private final int THUMB_START_ANGLE =90;
+    private final int THUMB_START_ANGLE =130;
+    private final int THUMB_FINAL_ANGLE =360;
+    private final int SWEEP_MAX =THUMB_FINAL_ANGLE-THUMB_START_ANGLE;
+    private final int SWEEP_MIN =0;
 
     private int mSweepedAngle=0;
 
@@ -76,13 +84,16 @@ public class ThumbView extends View{
             mShinnerPath.addArc(shinnerRectF, THUMB_START_ANGLE,mSweepedAngle);
             canvas.clipPath(mShinnerPath);
             Paint paint = new Paint();
-            paint.setStyle(Paint.Style.FILL);
+            int alpha = (int) ((mSweepedAngle * 1f) / SWEEP_MAX * 255);
+            Log.i(TAG, "mSweepedAngle:"+mSweepedAngle);
+            Log.i(TAG, "alpha:"+alpha);
+            paint.setAlpha(Math.max(alpha,0));//alpha不小于0
             canvas.drawBitmap(mShinerBitmap, 0, 0, paint);
             canvas.restore();
 
             canvas.drawBitmap(mNotThumbBitmap, 0 - dpToPx(2), mShinerBitmap.getHeight() - dpToPx(8), new Paint());
 
-            if(THUMB_START_ANGLE+mSweepedAngle>100){
+            if(mSweepedAngle>SWEEP_MIN){
                 mSweepedAngle-=10;
                 invalidate();
             }
@@ -90,16 +101,21 @@ public class ThumbView extends View{
         }else if(status==THUMB) {
             canvas.save();
             mShinnerPath=new Path();
-            RectF shinnerRectF = new RectF(0f, 0f, mShinnerWidth + dpToPx(2), mShinnerHegiht + dpToPx(2));
+            RectF shinnerRectF = new RectF(0f, 0f, mShinnerWidth + dpToPx(2), mShinnerHegiht + dpToPx(10));
 
             mShinnerPath.addArc(shinnerRectF, THUMB_START_ANGLE,mSweepedAngle);
             canvas.clipPath(mShinnerPath);
-            canvas.drawBitmap(mShinerBitmap, 0, 0, new Paint());
+            Paint paint = new Paint();
+            int alpha = (int) ((mSweepedAngle * 1f) / SWEEP_MAX * 255);
+            Log.i(TAG, "mSweepedAngle:"+mSweepedAngle);
+            Log.i(TAG, "alpha:"+alpha);
+            paint.setAlpha(Math.min(alpha,255));//alpha不能超过255
+            canvas.drawBitmap(mShinerBitmap, 0, 0, paint);
             canvas.restore();
 
             canvas.drawBitmap(mThumbBitmap, 0 - dpToPx(2), mShinerBitmap.getHeight() - dpToPx(8), new Paint());
 
-            if(THUMB_START_ANGLE +mSweepedAngle<=360) {
+            if(mSweepedAngle<SWEEP_MAX) {
                 mSweepedAngle+=10;
                 invalidate();
             }
